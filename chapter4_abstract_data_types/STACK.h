da1e7ef8883d3a13b77f91da79b899edb17faeaf
zhiqiangfang <zhiqiang.fang@foxmail.com>
2019-04-08 21:12:41 +0800

    update

---
+++ chapter4_abstract_data_types/STACK.h
@@ -0,0 +1,6 @@
+// program 4.1 Pushdown-stack ADT interface
+
+void STACKinit(int);
+int STACKempty(void);
+void STACKpush(Item);
+Item STACKpop();
---
+++ chapter4_abstract_data_types/program_4.2.c
@@ -0,0 +1,26 @@
+// Postfix-expression evaluation
+
+#include <stdio.h>
+#include <string.h>
+#include "STACK.h"
+
+int main(int argc, char *argv[])
+{
+    char *a = argv[1];
+    int i, N = strlen(a);
+    STACKinit(N);
+    for (i = 0; i < N; i++) {
+        if (a[i] == '+')
+            STACKpush(STACKpop() + STACKpop());
+        if (a[i] == '*')
+            STACKpush(STACKpop() * STACKpop());
+        if ((a[i] >= '0') && (a[i] <= '9'))
+            STACKpush(0);
+        while ((a[i] >= '0') && (a[i] <= '9'))
+            STACKpush(STACKpop() * 10 + (a[i++] - '0'));
+    }
+
+    printf("%d \n", STACKpop());
+
+    return 0;
+}
